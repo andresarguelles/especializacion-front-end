e390b8c438f76653ec3602e687a24d0801aac908
andresarguelles <aarguellesgonzalez@gmail.com>
2025-02-16 01:36:09 -0600

    Agregando archivos de clase 1

---
+++ Clase1/index.js
@@ -0,0 +1,6 @@
+var numero1 = parseInt(prompt("Digite o primeiro número: "));
+var numero2 = parseInt(prompt("Digite o segundo número: "));
+var numero3 = parseInt(prompt("Digite o terceiro número: "));
+
+//Mostramos la suma en pantalla con una alerta
+alert("La suma de los tres números es: " + (numero1 + numero2 + numero3));
